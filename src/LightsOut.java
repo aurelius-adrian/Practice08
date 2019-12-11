dccbebb1d142c7d09363a75608eb1132166cb407
aurelius <aurelius.adrian.info@gmail.com>
2019-12-11 15:56:22 +0100

    Practice08 (2)

--- src/LightsOut.java
+++ src/LightsOut.java
@@ -1,6 +1,40 @@
 public class LightsOut {
 
-    LightsOut (int cols, int  rows, long state, long mask) {
-        state = BitOps.clear(state, (int) mask);
+    public long state;
+    public int rows, cols;
+
+    public LightsOut(int cols, int rows, long state, long mask) {
+        long stateTmp = (BitOps.clear(state, (int) mask)) << (64 - (rows * cols)) >>> (64 - (rows * cols));
+        this.state = stateTmp;
+        this.rows = rows;
+        this.cols = cols;
+    }
+
+    public long getState() {
+        return state;
+    }
+
+    public void toggle(int col, int row) {
+        long changeMask = 0;
+        int position = ((row - 1) * row) + (col - 1);
+        for (int i = cols * rows; i > 0; i--) {
+            if (i == position) {
+                if (0 == (i - 1) % cols || 0 == i % cols) {
+                    changeMask <<= 2;
+                    changeMask |= 0b11;
+                    i--;
+                } else {
+                    changeMask <<= 3;
+                    changeMask |= 0b111;
+                    i -= 2;
+                }
+            } else if (i == position - (col - 1) || i == position + (col - 1)) {
+                changeMask <<= 1;
+                changeMask |= 0b1;
+            } else {
+                changeMask <<= 1;
+            }
+        }
+        this.state = BitOps.flip (state, (int) changeMask);
     }
 }
