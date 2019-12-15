affc747b7f5324633b999efed2ce2c5a667f4cef
aurelius <aurelius.adrian.info@gmail.com>
2019-12-15 14:59:30 +0100

    Practice08 (5)

--- src/BitOps.java
+++ src/BitOps.java
@@ -1,11 +1,19 @@
 public class BitOps {
     public static long set (long bitSet, int bitIndex) {
-        return (bitSet | bitIndex);
+        long mask = 1;
+        for (int i = 0; i < bitIndex; i++) {
+            mask <<= 1;
+        }
+        return (bitSet | mask);
     }
 
     public static boolean isSet (long bitSet, int bitIndex) {
-        bitSet = bitSet & bitIndex;
-        if (bitSet == bitIndex) {
+        long mask = 1;
+        for (int i = 0; i < bitIndex; i++) {
+            mask <<= 1;
+        }
+        bitSet = bitSet & mask;
+        if (bitSet == mask) {
             return true;
         } else {
             return false;
@@ -13,11 +21,15 @@ public class BitOps {
     }
 
     public static long clear (long bitSet, int bitIndex) {
-        return bitIndex & bitSet;
+        return flip(-1, bitIndex) & bitSet;
     }
 
     public static long flip (long bitSet, int bitIndex) {
-        return (bitSet ^ bitIndex);
+        long mask = 1;
+        for (int i = 0; i < bitIndex; i++) {
+            mask <<= 1;
+        }
+        return (bitSet ^ mask);
     }
 
     public ZahlenFolgenMerker solve() {
